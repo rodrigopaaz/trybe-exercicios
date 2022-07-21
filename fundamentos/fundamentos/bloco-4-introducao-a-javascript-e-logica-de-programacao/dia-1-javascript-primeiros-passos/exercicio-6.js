eefbbf989226af9f98f58ac4c8d8cbe5902d9539
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-07-20 22:46:18 -0300

    finalizando os exercicios 4.1

---
+++ fundamentos/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-6.js
@@ -0,0 +1,10 @@
+let bishop = "Frente";
+
+bishop.toLowerCase();
+
+if (bishop !== "diagonals") {
+    console.log ("Movimento Inválido!")
+}
+else {
+    console.log ("Movimento Realizado com Sucesso!")
+}
