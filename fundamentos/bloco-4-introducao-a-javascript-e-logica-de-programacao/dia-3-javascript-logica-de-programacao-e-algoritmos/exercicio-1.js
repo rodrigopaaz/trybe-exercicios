3f2a0c35dc5804add54865b047157603f458f355
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-07-22 20:24:53 -0300

    Realização dos exercicios 4.3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1.js
@@ -0,0 +1,46 @@
+//exercicio-1
+/*
+let number=1;
+
+for (let index = 1; index < 10 ; index += 1){
+    number *= index;
+}
+console.log(number);
+*/
+
+//exercício-2
+
+/*let word = ('tryber');
+let reverseWord = [];
+
+for (let index=5; index >= 0; index-=1) {
+    reverseWord.push(word[index])
+}
+console.log(reverseWord);
+*/
+
+//exercicio-3
+
+let counter = "";
+let counter2= 0;
+let result = 0;
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+    for (let index=5; index >= 5; index-=1) {
+    counter = array[index]; 
+    console.log(counter)
+    }
+    /*counter2 = counter.lenght;
+        if (result < counter2) {
+              result = counter2  
+        } else {result = result}
+}
+        console.log (counter);
+
+
+/*3- Considere o array de strings abaixo:
+Copiar
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+*/
