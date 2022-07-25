c3a1f07788c9f03a55ed8b11e08f5ad4e34031e3
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-07-25 16:17:39 -0300

    exercicios 4.4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
@@ -0,0 +1,26 @@
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+  //exercicio-1
+
+//    console.log('Bem vinda', info.personagem)
+    
+
+//exercicio-2
+
+        //console.table(info);
+
+
+//exercicio-3
+
+for (let index in info) {
+    console.log(info[index])
+}
+
+//exercicios-4
+
