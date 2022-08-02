366728d5ea48578de75cd0ac139d48b458f36e2e
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-08-02 00:00:14 -0300

    Adicionando funcoes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -2,26 +2,31 @@ let matriz = document.getElementsByTagName('h1')[0].parentElement
 
 botaoCor = document.getElementById('changeBackground')
 botaoCor.addEventListener('click', function (){
-   matriz.style.background = 'red' 
+    if(matriz.style.background == 'purple'){matriz.style.background ='red'} 
+    else {matriz.style.background ='purple'} 
 })
 
-botaoTextColor = document.getElementById('changeTextColor"')
+botaoTextColor = document.getElementById('changeTextColor')
 botaoTextColor.addEventListener('click', function (){
-    matriz.style.color = 'blue' 
+    if(matriz.style.color == 'blue'){matriz.style.color ='green'} 
+    else {matriz.style.color ='blue'} 
  })
 
 
 botaochangeText= document.getElementById('changeText')
 botaochangeText.addEventListener('click', function (){
-    matriz.style.background = 'red' 
+    if(matriz.style.fontSize == '16px'){matriz.style.fontSize ='30px'} 
+    else {matriz.style.fontSize ='16px'} 
  })
 botaochangeSpace = document.getElementById('changeSpace')
 botaochangeSpace.addEventListener('click', function (){
-    matriz.style.background = 'red' 
+    if(matriz.style.wordSpacing == '10px'){matriz.style.wordSpacing ='15px'} 
+    else {matriz.style.wordSpacing ='10px'} 
  })
 botaochangeFont= document.getElementById('changeFont')
 botaochangeFont.addEventListener('click', function (){
-    matriz.style.background = 'red' 
+    if(matriz.style.fontFamily == 'Courier'){matriz.style.fontFamily = 'sans-serif'} 
+    else {matriz.style.fontFamily = 'Courier'} 
  })
 
 {/* <button id="changeBackground">Mudar Cor de fundo da tela</button>
