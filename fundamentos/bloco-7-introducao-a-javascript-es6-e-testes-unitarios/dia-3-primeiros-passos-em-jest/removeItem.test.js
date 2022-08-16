51094756d8721f5debb682cbfc9a088911c09ce3
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-08-16 20:18:44 -0300

    Iniciando o exercicio 7.3

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/removeItem.test.js
@@ -0,0 +1,9 @@
+const myRemove = require('.removeItem.js');
+const { it } = require('node:test');
+const { describe } = require('yargs');
+
+describe('testando array:', () => {
+    it('verifica se o array contém a informação', () => {
+        expect()
+    })
+})
---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,6 @@
+const sum = require('./sum.js');
+
+test('Verifica de 0 + 0 são 0', () => {
+    expect(sum(0,'0')).toBe(0) })
+
+
