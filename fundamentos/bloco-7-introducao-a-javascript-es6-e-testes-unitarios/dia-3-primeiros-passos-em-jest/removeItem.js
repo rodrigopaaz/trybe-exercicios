bd4df8fecd7617607bdd119c99d392239c6e27fd
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-08-17 11:43:16 -0300

    realizado até o exercicio 4

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/removeItem.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/removeItem.js
@@ -8,6 +8,7 @@ function myRemove(arr, item) {
     return newArr;
 }
 
+module.exports = myRemove;
 
 
 
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/removeItem.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/removeItem.test.js
@@ -1,9 +1,9 @@
-const myRemove = require('.removeItem.js');
-const { it } = require('node:test');
-const { describe } = require('yargs');
+const myRemove = require('./removeItem');
+/* const { it } = require('node:test');
+const { describe } = require('yargs'); */
 
 describe('testando array:', () => {
     it('verifica se o array contém a informação', () => {
-        expect()
+        expect(myRemove([1,2,3,4],5)).toStrictEqual([1,2,3,4]) 
     })
 })
