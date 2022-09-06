17cbb31de7f39daa221425551319749eb41ce30a
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-09-06 19:34:51 -0300

    realizado ate o exercicio 5

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -0,0 +1,123 @@
+//EXERCICIO 1
+/* const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} está a ${value} ${measurementUnit} de distância do Sol`;
+
+const MEASUREMENT_UNIT = 'quilômetros';
+
+const mars = {
+  name: 'Marte',
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const venus = {
+  name: 'Venus',
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const jupiter = {
+  name: 'Jupiter',
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+console.log(planetDistanceFromSun(venus)); // B
+console.log(planetDistanceFromSun(jupiter)); // C */
+
+//EXERCICIO 2
+
+/* const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} está a ${value} ${measurementUnit} de distância do Sol`;
+
+const MEASUREMENT_UNIT = 'quilômetros';
+
+const mars = {
+  name: 'Marte',
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const venus = {
+  name: 'Venus',
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const jupiter = {
+  name: 'Jupiter',
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C */
+
+//EXERCICIO 3
+
+/* const getPlanet = () => {
+    const mars = {
+      name: 'Mars',
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: 'kilometers',
+      },
+    };
+    console.log('Returned planet: ', mars);
+  };
+  const time = 4000
+  setTimeout(() => getPlanet(), time) */
+
+  //EXERCICIO 4
+
+/*   const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+ */
+//EXERCICIO 4
+
+// crie a função sendMarsTemperature abaixo
+//setTimeout(() => console.log(getMarsTemperature()), messageDelay)
+
+/* //EXERCICIO 5
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
+// Definição da função sendMarsTemperature...
+
+const sendMarsTemperature = (parametro) =>  parametro;
+
+sendMarsTemperature(temperatureInFahrenheit(getMarsTemperature(toFahrenheit))); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+
+sendMarsTemperature(greet(getMarsTemperature())); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo */
+
