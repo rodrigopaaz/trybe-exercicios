a0dafc9bd69ecf0b3aa7d39c2b610a8c8c820112
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-09-08 13:54:49 -0300

    realizando o requisito 7

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -121,3 +121,82 @@ sendMarsTemperature(temperatureInFahrenheit(getMarsTemperature(toFahrenheit))); 
 
 sendMarsTemperature(greet(getMarsTemperature())); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo */
 
+//EXERCICIO 6
+/* 
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// definição da função sendMarsTemperature...
+const didOperationSucceed = Math.random() >= 0.5;
+
+const sendMarsTemperature = (parametro1, parametro2) => {
+  setTimeout(() => {
+  if(didOperationSucceed){
+    return parametro1(temperatureInFahrenheit(getMarsTemperature()))
+  }
+  return parametro2(handleError('Robot is Busy'))
+},messageDelay())
+}
+
+
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(toFahrenheit(getMarsTemperature), handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError); */
+
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grama',
+    ability: 'Raio Solar',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fogo',
+    ability: 'Lança Chamas',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Água',
+    ability: 'Jato de Água',
+  },
+];
+
+function getPokemonDetails(selectedPokemon, callback) {
+  const foundPokemon = pokemons.find((pokemon) => pokemon.name === selectedPokemon);
+
+  setTimeout(() => {
+    if (foundPokemon === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+
+    const { name, type, ability } = foundPokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+const handlePokemonSearch = (error, message) => {
+  return message
+};
+
+getPokemonDetails('Squirtle', handlePokemonSearch);
